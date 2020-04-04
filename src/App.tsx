6c6a88698c3e7b80df70cedeb90c43f7841f302c
Isaac Datlof <ike18t@gmail.com>
2020-04-03 23:46:02 -0400

    feat: add css transformations to key

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as Key } from "tabler-icons/icons/key.svg";
 
 export function App() {
   const [validForm] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -11,7 +12,7 @@ export function App() {
         <input type="email" placeholder="Email Address" autoComplete="username" />
         <span className={styles.passwordContainer}>
           <input className="password" type="password" placeholder="Password" autoComplete="new-password" />
-          <Key className={styles.key} />
+          <Key className={passwordsMatch ? styles.matchingKey : styles.key} />
         </span>
         <span className={styles.passwordContainer}>
           <input
@@ -20,10 +21,11 @@ export function App() {
             placeholder="Confirm Password"
             autoComplete="new-password"
           />
-          <Key className={styles.key} />
+          <Key className={passwordsMatch ? styles.matchingKey : styles.key} />
         </span>
         <input disabled={!validForm} type="submit" value="Sign Up!" />
       </form>
+      <button onClick={() => setPasswordsMatch(!passwordsMatch)}>hi</button>
     </div>
   );
 }
